08593cf30b5125e15c480e398725fbc91e5b1d2f
IvoPaunov <ivo.paunov@gmail.com>
2022-06-15 13:55:08 +0300

    initial cfg with CRA and grommet

---
+++ src/theme.js
@@ -0,0 +1,14 @@
+const theme = {
+    global: {
+        colors: {
+            brand: '#34344E',
+        },
+        font: {
+            family: 'Open Sans',
+            size: '18px',
+            height: '20px',
+        },
+    },
+};
+
+export default theme
