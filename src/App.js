1fdbf48931b6d0040620164e69ce4bcf3951645b
IvoPaunov <ivo.paunov@gmail.com>
2022-06-15 15:14:34 +0300

    add root

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
 import { Grommet } from 'grommet';
 import theme from './theme';
+import Root from './Root'
 
 
 function App() {
   return (
-    <Grommet theme={theme} themeMode='dark'>
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Grommet theme={theme} themeMode='dark' full>
+      <Root />
     </Grommet>
   );
 }
---
+++ src/Root.js
@@ -0,0 +1,19 @@
+import {
+    Box,
+    Paragraph
+} from "grommet";
+
+function Root() {
+    return (
+        <Box background="background-back" fill pad="medium" gap="medium">
+            <Box width="medium" background="background-front" round='small'>
+                <Box pad="medium">
+                    <Paragraph>Ambire AdEx</Paragraph>
+                </Box>
+
+            </Box>
+        </Box>
+    );
+}
+
+export default Root;
