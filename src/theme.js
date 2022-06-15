2da1505f9e8b061542f7bbe4fa34cea6a1a316ff
IvoPaunov <ivo.paunov@gmail.com>
2022-06-15 15:14:24 +0300

    test theme

--- src/theme.js
+++ src/theme.js
@@ -1,14 +1,41 @@
-const theme = {
+import { grommet } from "grommet";
+import { deepMerge } from "grommet/utils";
+
+const theme = deepMerge(grommet, {
     global: {
         colors: {
             brand: '#34344E',
+            "background-back": {
+                dark: "linear-gradient(180deg, #3E436B 0%, #24263D 100%)",
+                light: "#EFEFEF"
+            },
+            "background-front": {
+                dark: "linear-gradient(180deg, #2D314D 0%, #272A4200 100%)",
+                light: "#FFFFFF"
+            },
+            "background-contrast": {
+                dark: "#FFFFFF08",
+                light: "#11111108"
+            },
+            "background-custom": {
+                dark: "#0E5265",
+                light: "#00C8FF"
+            }
         },
         font: {
-            family: 'Open Sans',
+            family: 'Poppins',
             size: '18px',
             height: '20px',
         },
     },
-};
+    box: {
+        border: {
+            radius: '13px'
+        }
+    }
+});
+
+console.log({ theme })
+console.log({ grommet })
 
 export default theme
