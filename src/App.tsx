e99874f1216e649e0b70ce5f8405cb15515efd89
IvoPaunov <ivo.paunov@gmail.com>
2022-06-16 17:55:55 +0300

    add gnosis web3 provider to account context

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,17 @@ import { Grommet } from 'grommet';
 import theme from './theme';
 import Root from './Root'
 import AccountProvider from 'components/AccountProvider/AccountProvider'
+import SafeProvider from 'lib/safe-apps-react-sdk'
 
 
 function App() {
   return (
     <Grommet theme={theme} themeMode='dark' full>
-      <AccountProvider>
-        <Root />
-      </AccountProvider>
+      <SafeProvider>
+        <AccountProvider>
+          <Root />
+        </AccountProvider>
+      </SafeProvider>
     </Grommet>
   );
 }
--- src/components/AccountProvider/AccountProvider.tsx
+++ src/components/AccountProvider/AccountProvider.tsx
@@ -1,14 +1,19 @@
-import { createContext, useState, FC, PropsWithChildren } from 'react'
+import { createContext, useState, FC, PropsWithChildren, useMemo } from 'react'
+import { ethers, providers } from 'ethers'
+import { SafeAppProvider } from '@gnosis.pm/safe-apps-provider'
+import { useSafeAppsSDK } from 'lib/safe-apps-react-sdk'
 
 interface IAccountContext {
     id: string | null,
-    name: string | null
-
+    name: string | null,
+    provider: providers.Web3Provider
 }
 
 const AccountContext = createContext<IAccountContext | null>(null)
 
 const AccountProvider: FC<PropsWithChildren> = ({ children }) => {
+    const { sdk, safe } = useSafeAppsSDK()
+    const provider = useMemo(() => new ethers.providers.Web3Provider(new SafeAppProvider(safe, sdk)), [sdk, safe])
 
     const [id, setId] = useState(null)
     const [name, setName] = useState(null)
@@ -16,7 +21,8 @@ const AccountProvider: FC<PropsWithChildren> = ({ children }) => {
     return (<AccountContext.Provider
         value={{
             id,
-            name
+            name,
+            provider
         }}
     >
         {children}
