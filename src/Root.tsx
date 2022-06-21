b11d59d8539b7834d15e3f8c3ffce7dd37dbd11f
IvoPaunov <ivo.paunov@gmail.com>
2022-06-21 17:48:41 +0300

    update root routes

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,11 +1,32 @@
 
-import { HashRouter as Router } from "react-router-dom"
-import Platform from "components/Platform/Platform";
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import Platform from 'components/Platform/Platform'
+import AccountSelector from 'components/AccountSelector/AccountSelector'
+import { useAccount } from 'hooks'
 
 function Root() {
+
+    const { authenticated } = useAccount()
+
     return (
         <Router>
-            <Platform />
+            <Routes>
+                <Route path='/account-select'
+                    element={<AccountSelector />}
+                />
+                <Route path='/platform'
+                    element={
+                        authenticated ?
+                            <Platform />
+                            : <Navigate replace to='/account-select' />
+                    }
+                />
+                <Route path="/"
+                    element={
+                        <Navigate replace to={authenticated ? '/platform' : '/account-select'} />
+                    }
+                />
+            </Routes>
         </Router>
 
     );
