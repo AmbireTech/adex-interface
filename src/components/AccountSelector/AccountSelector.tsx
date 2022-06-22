9db88891d4cd9de0f06e66d4671556feb1c7c722
IvoPaunov <ivo.paunov@gmail.com>
2022-06-22 12:25:37 +0300

    work on account selector

--- src/components/AccountSelector/AccountSelector.tsx
+++ src/components/AccountSelector/AccountSelector.tsx
@@ -1,19 +1,50 @@
-import { FC } from 'react'
-import { Box, Tag } from 'grommet'
+import { FC, useState } from 'react'
+import { Box, Tag, Select } from 'grommet'
 import { useAccount } from 'hooks'
 import { shortenedAddress } from 'lib/formatters'
+import { IAdExAccount } from 'types'
 
 interface IAccountSelector {
 }
 
 const AccountSelector: FC<IAccountSelector> = () => {
 
-    const { identity } = useAccount()
+    const { identity, availableAdexAccounts } = useAccount()
+    const [selected, setSelected] = useState<IAdExAccount | null>(null)
 
     return (
         <Box>
             <Tag name="Connected wallet" value={identity ? shortenedAddress(identity) : 'Not connected'} />
-        </Box>
+            <Select
+                options={availableAdexAccounts}
+                value={
+                    <Box wrap direction="row" width="small"> {
+                        selected ?
+                            <Tag name={selected.name} value={shortenedAddress(selected.adexIdentity)} />
+
+                            :
+                            <Box
+                                pad={{ vertical: 'xsmall', horizontal: 'small' }}
+                                margin="xsmall"
+                            >
+                                Select AdEx identity
+                            </Box>
+                    }
+
+                    </Box>
+
+                }
+                onChange={(nextSelected) => {
+                    setSelected(nextSelected.value)
+                }}
+            >
+                {({ name, adexIdentity }, state) =>
+                    <Box pad="small" background={state.active ? 'active' : undefined}>
+                        <Tag name={name} value={shortenedAddress(adexIdentity)} />
+                    </Box>
+                }
+            </Select >
+        </Box >
     )
 }
 
