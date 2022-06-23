099ca9bfb1da17492917233653dc065e668f9158
IvoPaunov <ivo.paunov@gmail.com>
2022-06-23 17:11:52 +0300

    Move router to app level

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import theme from './theme';
 import Root from './Root'
 import AccountProvider from 'components/providers/AccountProvider/AccountProvider'
 import SafeProvider from 'lib/safe-apps-react-sdk'
+import { HashRouter as Router } from 'react-router-dom'
 
 
 function App() {
@@ -11,7 +12,9 @@ function App() {
     <Grommet theme={theme} themeMode='dark' full>
       <SafeProvider>
         <AccountProvider>
-          <Root />
+          <Router>
+            <Root />
+          </Router>
         </AccountProvider>
       </SafeProvider>
     </Grommet>
