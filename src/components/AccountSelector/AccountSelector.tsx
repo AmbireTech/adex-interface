07c7078282bc2c78fc6842eaa672549e6dd2cff4
IvoPaunov <ivo.paunov@gmail.com>
2022-06-30 18:02:21 +0300

    work on account selector

--- src/components/AccountSelector/AccountSelector.tsx
+++ src/components/AccountSelector/AccountSelector.tsx
@@ -1,6 +1,6 @@
-import { FC, useState } from 'react'
+import { FC, useState, useCallback } from 'react'
 import { Box, Tag, Select, Form, TextInput, FormField, RadioButtonGroup, Button } from 'grommet'
-import { useAccount } from 'hooks'
+import { useAccount, useToasts } from 'hooks'
 import { shortenedAddress } from 'lib/formatters'
 import { IAdExAccount, AccountType } from 'types'
 import { useNavigate } from "react-router-dom"
@@ -9,31 +9,54 @@ interface IAccountSelector { }
 
 const AccountOption = ({ adexAccount }: { adexAccount: IAdExAccount | null }) =>
     <Box pad='small' gap='small' direction='row' flex={{ grow: 1 }} >
-        {adexAccount && <Box>{adexAccount.name}:</Box>}
+        {adexAccount && <Box>{adexAccount.email}:</Box>}
         {adexAccount && <Box>{shortenedAddress(adexAccount?.adexIdentity || '...')}</Box>}
         {!adexAccount && 'Select AdEx identity'}
     </Box>
 
 const AccountSelector: FC<IAccountSelector> = () => {
     const navigate = useNavigate()
-    const { identity, availableAdexAccounts, registerAdexUser } = useAccount()
+    const { addToast } = useToasts()
+    const { identity, availableAdexAccounts, registerAdexUser, selectAdexUser, authenticated, adexAccount } = useAccount()
     const [selected, setSelected] = useState<IAdExAccount | null>(null)
     const [loading, setLoading] = useState(false)
+    const [newOpen, setNewOpen] = useState(false)
 
     const onSubmit = async ({ value }: { value: IAdExAccount }) => {
         setLoading(true)
         const { error } = await registerAdexUser(value)
         setLoading(false)
 
-        if(!error) navigate('/')
+        if (!error) {
+            navigate('/platform/dashboard')
+            addToast('AdEx Account registered!', { sticky: true, status: 'ok' })
+        }
     }
 
+    const onLogAs = useCallback(() => {
+        const user = selectAdexUser(selected?.adexIdentity || null)
+        if (user) {
+            navigate('/platform/dashboard')
+            addToast(`Logged as ${user.email}`, { status: 'ok' })
+        }
+    }, [addToast, navigate, selectAdexUser, selected?.adexIdentity])
+
+    const onContinueAs = useCallback(() => {
+        navigate('/platform/dashboard')
+    }, [navigate])
 
     return (
         <Box>
             <Tag name='Connected wallet' value={identity ? shortenedAddress(identity) : 'Not connected'} />
 
-            {!availableAdexAccounts.length ?
+            {authenticated && adexAccount &&
+                <Button
+                    label={`Continue as ${adexAccount.email}`}
+                    onClick={onContinueAs}
+                />
+            }
+
+            {!!availableAdexAccounts.length &&
                 <Select
                     width='full'
                     options={availableAdexAccounts}
@@ -44,36 +67,46 @@ const AccountSelector: FC<IAccountSelector> = () => {
                 >
                     {(adexAccount) => <AccountOption adexAccount={adexAccount} />}
                 </Select >
-                :
-                <Box pad='small'>
-                    <Box>Create AdEx account</Box>
-                    <Form onSubmit={onSubmit}>
-                        <FormField label='Name' name='name' required>
-                            <TextInput name='name' />
-                        </FormField>
-                        <FormField label='Email' name='email' required
-                            validate={{ regexp: /^\w+([+\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ }}
-                        >
-                            <TextInput name='email' />
-                        </FormField>
-                        <FormField name='role' label='Account type' required>
-                            <RadioButtonGroup name='role' options={[
-                                {
-                                    id: AccountType.ADVERTISER.toString(),
-                                    value: AccountType.ADVERTISER,
-                                    label: 'Advertiser'
-                                },
-                                {
-                                    id: AccountType.PUBLISHER.toString(),
-                                    value: AccountType.PUBLISHER,
-                                    label: 'Publisher'
-                                }
-                            ]} />
-                        </FormField>
-                        <Button type='submit' label='Create' primary disabled={loading} />
-                    </Form>
-                </Box>
             }
+
+            {!!selected && <Button
+                label={`Log as ${selected.email}`}
+                onClick={onLogAs}
+            />}
+
+            {!!availableAdexAccounts.length &&
+                <Box>OR</Box>
+            }
+
+            <Box pad='small' hidden={!(newOpen || !availableAdexAccounts.length)}>
+                <Box>Create AdEx account</Box>
+                <Form onSubmit={onSubmit}>
+                    <FormField label='Name' name='name' required>
+                        <TextInput name='name' />
+                    </FormField>
+                    <FormField label='Email' name='email' required
+                        validate={{ regexp: /^\w+([+\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ }}
+                    >
+                        <TextInput name='email' />
+                    </FormField>
+                    <FormField name='role' label='Account type' required>
+                        <RadioButtonGroup name='role' options={[
+                            {
+                                id: AccountType.ADVERTISER.toString(),
+                                value: AccountType.ADVERTISER,
+                                label: 'Advertiser'
+                            },
+                            {
+                                id: AccountType.PUBLISHER.toString(),
+                                value: AccountType.PUBLISHER,
+                                label: 'Publisher'
+                            }
+                        ]} />
+                    </FormField>
+                    <Button type='submit' label='Create' primary disabled={loading} />
+                </Form>
+            </Box>
+
         </Box >
     )
 }
