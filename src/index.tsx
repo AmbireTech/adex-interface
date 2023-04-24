f02ece60d8769584f1cb90118288d7e40cf5705a
IvoPaunov <ivo.paunov@gmail.com>
2023-04-24 16:56:27 +0300

    add mantine provider and remove default bp stuff

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
+import { MantineProvider } from '@mantine/core'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <App />
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <App />
+    </MantineProvider>
   </React.StrictMode>
 )
 
