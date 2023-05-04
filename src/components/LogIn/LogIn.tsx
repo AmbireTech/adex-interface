14818ab5d8efef4e596a0b9162dc921a772820d9
IvoPaunov <ivo.paunov@gmail.com>
2023-05-04 18:03:55 +0300

    wip: login layout

--- src/components/LogIn/LogIn.tsx
+++ src/components/LogIn/LogIn.tsx
@@ -1,15 +1,21 @@
 import useAccount from 'hooks/useAccount'
-import { Grid, Button } from '@mantine/core'
+import { Container, Grid, Button, Center } from '@mantine/core'
 
 function LogIn() {
   const { connectWallet, adexAccount } = useAccount()
   return (
-    <Grid>
-      <Grid.Col span={4}>SOme image and title here</Grid.Col>
-      <Grid.Col span={2}>
-        <Button onClick={connectWallet}>{`Test account conect ${adexAccount?.address}`}</Button>
-      </Grid.Col>
-    </Grid>
+    <Container fluid h="100vh">
+      <Grid grow h="100%" justify="stretch">
+        <Grid.Col md={8} order={1} xs={12} orderXs={2} orderMd={1}>
+          <Center h="100%">Some image and title here</Center>
+        </Grid.Col>
+        <Grid.Col md={4} order={2} xs={12} orderXs={1} orderMd={2}>
+          <Center h="100%">
+            <Button onClick={connectWallet}>{`Test account conect ${adexAccount?.address}`}</Button>
+          </Center>
+        </Grid.Col>
+      </Grid>
+    </Container>
   )
 }
 
