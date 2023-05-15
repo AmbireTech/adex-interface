0c1e6f0a81f353a845d06f25488ff99215aeea9c
IvoPaunov <ivo.paunov@gmail.com>
2023-05-15 17:39:03 +0300

    add example links to side nav

--- src/components/SideNav/SideNav.tsx
+++ src/components/SideNav/SideNav.tsx
@@ -1,17 +1,19 @@
 import { Navbar, ScrollArea, Box } from '@mantine/core'
 import useAccount from 'hooks/useAccount'
+import { useNavigate } from 'react-router-dom'
 import NavLink from './NavLink'
 
 function SideNav() {
   const { connectWallet, disconnectWallet, adexAccount } = useAccount()
+  const navigate = useNavigate()
   return (
     <Navbar width={{ base: 210 }} height="100%" p="xs">
       <Navbar.Section mt="xs">AdEx logo here</Navbar.Section>
       <Navbar.Section mt="xs" grow component={ScrollArea} mx="-xs" px="xs">
         <Box py="md">
-          <NavLink label="Dashboard" />
+          <NavLink label="Dashboard" action={() => navigate('/dashboard')} />
           <NavLink label="Deposit" />
-          <NavLink label="Billing" />
+          <NavLink label="Billing" action={() => navigate('billing')} />
           <NavLink label="Help Center" />
           <NavLink label={`Test account login ${adexAccount?.address}`} action={connectWallet} />
           <NavLink
