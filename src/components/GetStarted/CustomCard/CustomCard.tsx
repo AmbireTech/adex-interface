ab552f817566e3d867a4067bec5b2bc2920d0bba
Lyubomir Nanev <lyubomir.nanev@gmail.com>
2023-06-02 11:57:34 +0300

    Modified CustomCard component

--- src/components/GetStarted/CustomCard/CustomCard.tsx
+++ src/components/GetStarted/CustomCard/CustomCard.tsx
@@ -1,98 +1,103 @@
-import {
-  Button,
-  Box,
-  Group,
-  Text,
-  Flex,
-  rem,
-  Title,
-  createStyles,
-  ThemeIcon,
-  MantineColor
-} from '@mantine/core'
+import { Box, Group, Text, Flex, rem, Title, createStyles, MantineColor } from '@mantine/core'
 import { useHover } from '@mantine/hooks'
+import React, { CSSProperties } from 'react'
+import { SystemProp } from '@mantine/styles'
 
 interface StylesProps {
   color: MantineColor
   hovered: boolean
+  width: number
+  height: number
+  hasAction: boolean
 }
 
-const useStyles = createStyles((theme, { color, hovered }: StylesProps) => ({
-  wrapper: {
-    transition: theme.transitionTimingFunction,
-    backgroundColor: hovered
-      ? theme.fn.lighten(theme.colors[color][theme.fn.primaryShade()], 0.97)
-      : theme.white,
-    textAlign: 'center',
-    padding: theme.spacing.xl,
-    borderRadius: theme.radius.md,
-    height: rem(330),
-    width: rem(294),
-    border: '1px solid',
-    borderColor: hovered
-      ? theme.colors[color][theme.fn.primaryShade()] + theme.other.shades.hexColorSuffix.lighter
-      : theme.colors.gray[1],
-    boxShadow: hovered ? theme.shadows.md : ''
-  },
-  icon: {
-    transition: theme.transitionTimingFunction,
-    transform: hovered ? 'scale(1.5)' : 'scale(1)'
-  },
-  button: {
-    padding: '0'
-  }
-}))
+const useStyles = createStyles(
+  (theme, { color, hovered, width, height, hasAction }: StylesProps) => ({
+    wrapper: {
+      transition: `all ${theme.transitionTimingFunction}`,
+      textAlign: 'center',
+      padding: theme.spacing.xl,
+      borderRadius: theme.radius.md,
+      height: `${rem(height)}`,
+      width: `${rem(width)}`,
+      border: hovered
+        ? `1px solid ${theme.fn.lighten(
+            theme.colors[color][theme.fn.primaryShade()],
+            theme.other.shades.lighten.lighter
+          )}`
+        : 'transparent',
+      boxShadow: hovered ? theme.shadows.md : theme.shadows.xs,
+      cursor: hasAction ? 'pointer' : ''
+    },
+    icon: {
+      transition: theme.transitionTimingFunction,
+      transform: hovered && !hasAction ? 'scale(1.5)' : 'scale(1)'
+    },
+    button: {
+      padding: '0'
+    },
+    iconTest: {
+      display: 'flex',
+      alignItems: 'center',
+      color: theme.colors[color][theme.fn.primaryShade()]
+    }
+  })
+)
 
 const CustomCard = ({
   icon,
+  iconLeft,
   color,
   title,
   text,
-  buttonLabel,
+  width,
+  height,
+  children,
+  direction,
   action
 }: {
-  icon: React.ReactNode
+  icon?: React.ReactNode
+  iconLeft?: React.ReactNode
   color: MantineColor
-  title: string
+  title?: string
   text: string
-  buttonLabel: string
-  action: () => void
+  width: number
+  height: number
+  children?: React.ReactNode
+  direction: SystemProp<CSSProperties['flexDirection']>
+  action?: () => void
 }) => {
   const { hovered, ref } = useHover()
-  const { classes } = useStyles({ color, hovered })
+  const { classes } = useStyles({ color, hovered, width, height, hasAction: !!action })
 
   return (
-    <Box ref={ref} className={classes.wrapper}>
-      <Flex mih={50} gap="sm" justify="center" align="center" direction="column" wrap="wrap">
-        <Title mt="sm" color="gray.5" order={2}>
-          {title}
-        </Title>
-        <Box className={classes.icon}>
-          <ThemeIcon
-            variant="outline"
-            color={color}
-            size={rem(60)}
-            style={{ border: 'none', background: 'none' }}
-          >
-            {icon}
-          </ThemeIcon>
-        </Box>
-        <Group position="apart" mb="xs">
-          <Text size="lg" w={rem(160)}>
-            {text}
-          </Text>
-        </Group>
-        <Button
-          w="75%"
-          onClick={action}
-          size="lg"
-          variant="outline"
-          mt="sm"
-          color={color}
-          className={classes.button}
-        >
-          {buttonLabel}
-        </Button>
+    <Box ref={ref} className={classes.wrapper} onClick={action}>
+      <Flex mih={50} gap="sm" justify="center" align="center" direction={direction} wrap="wrap">
+        {title && (
+          <Title mt="sm" color="gray.5" order={2}>
+            {title}
+          </Title>
+        )}
+        {icon && (
+          <Box className={classes.icon}>
+            <span className={classes.iconTest}>{icon}</span>
+          </Box>
+        )}
+        {iconLeft && text ? (
+          <Group position="center">
+            <span className={classes.iconTest}>{iconLeft}</span>
+            <Text size="lg" align="start" maw={rem(160)}>
+              {text}
+            </Text>
+          </Group>
+        ) : (
+          <Group position="apart" mb="xs">
+            <Text size="lg" maw={rem(160)}>
+              {text}
+            </Text>
+          </Group>
+        )}
+        {children}
       </Flex>
     </Box>
   )
--- src/components/GetStarted/GetStarted.tsx
+++ src/components/GetStarted/GetStarted.tsx
@@ -1,13 +1,13 @@
-import { Container, Grid, Flex, Title } from '@mantine/core'
+import { Container, Grid, Flex, Title, Button } from '@mantine/core'
 import DepositIcon from 'resources/icons/Deposit'
 import CampaignIcon from 'resources/icons/Campaign'
 import CustomCard from './CustomCard'
 
 const GetStarted = () => {
   return (
-    <Container fluid h="100vh">
+    <Container>
       <Flex
-        h="60%"
+        mt="25%"
         mih={50}
         gap="sm"
         justify="space-around"
@@ -21,22 +21,69 @@ const GetStarted = () => {
           </Grid.Col>
           <Grid.Col md={1} order={1} xs={12}>
             <CustomCard
+              width={294}
+              height={330}
               title="Step 1"
               text="Add funds to your account"
-              icon={<DepositIcon />}
+              icon={<DepositIcon size="40px" />}
               color="brand"
-              buttonLabel="Add funds"
-              action={() => console.log('Add funds clicked')}
-            />
+              direction="column"
+            >
+              <Button
+                w="75%"
+                size="lg"
+                variant="outline"
+                mt="sm"
+                color="brand"
+                onClick={() => console.log('Add funds clicked')}
+              >
+                Add funds
+              </Button>
+            </CustomCard>
           </Grid.Col>
           <Grid.Col md={4} order={2} xs={12}>
             <CustomCard
+              width={294}
+              height={330}
               title="Step 2"
               text="Create new campaign"
-              icon={<CampaignIcon />}
+              icon={<CampaignIcon size="40px" />}
               color="secondary"
-              buttonLabel="Create a campaign"
-              action={() => console.log('Create a campaign clicked')}
+              direction="column"
+            >
+              <Button
+                w="75%"
+                size="lg"
+                variant="outline"
+                mt="sm"
+                color="secondary"
+                p="0"
+                onClick={() => console.log('Create a campaign clicked')}
+              >
+                Create a campaign
+              </Button>
+            </CustomCard>
+          </Grid.Col>
+          <Grid.Col md={4} order={3} xs={12}>
+            <CustomCard
+              width={300}
+              height={100}
+              text="Statments"
+              iconLeft={<CampaignIcon size="24px" />}
+              color="secondary"
+              direction="row"
+              action={() => console.log('Statement clicked')}
+            />
+          </Grid.Col>
+          <Grid.Col md={4} order={4} xs={12}>
+            <CustomCard
+              width={164}
+              height={164}
+              icon={<CampaignIcon size="60px" />}
+              text="Desktop"
+              color="brand"
+              direction="column"
+              action={() => console.log('Desktop clicked')}
             />
           </Grid.Col>
         </Grid>
