e862faa9cfc65df2dac8326084d476fda48033fe
Lyubomir Nanev <lyubomir.nanev@gmail.com>
2023-06-02 11:58:53 +0300

    Merge remote-tracking branch 'origin/development' into get-started-page

--- src/themes/light.tsx
+++ src/themes/light.tsx
@@ -6,28 +6,28 @@ export const lightTheme: MantineThemeOverride = {
   colorScheme: 'light',
   colors: {
     brand: [
-      '#D6C4FF',
-      '#AC88FF',
-      '#8854FF',
-      '#6A2AFF',
-      '#4A00FF',
-      '#3F00DA',
-      '#3600B9',
-      '#2E009D',
-      '#270086',
-      '#210072'
+      '#ECEFF7',
+      '#ABBAE1',
+      '#6C89D8',
+      '#2C5CDE', // Primary
+      '#1E48BE', // Secondary
+      '#2B4077',
+      '#283457',
+      '#232A40',
+      '#1D2230',
+      '#181B24'
     ],
     secondary: [
-      '#A3FFFC',
-      '#3DFFF9',
-      '#00FFF1',
-      '#00C0B2',
-      '#008C82',
-      '#00625B',
-      '#004540',
-      '#00302D',
-      '#00221F',
-      '#001816'
+      '#C1CAE3',
+      '#8196D1',
+      '#4368CD',
+      '#1E48BE', // Secondary
+      '#233C84',
+      '#22315D',
+      '#1E2842',
+      '#1A2030',
+      '#151923',
+      '#111319'
     ]
     // NOTE: needs 10 shades to work https://mantine.dev/theming/colors/
     // attention: ['#FFBE4D', '#FFBE4D', '#FFBE4D'],
