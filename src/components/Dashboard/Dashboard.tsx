13c08abdf21b9f7074888e4fb754fa999a09cbda
Kristiyan Stefanov <46137735+KristiyanStefanov23@users.noreply.github.com>
2024-03-18 13:52:58 +0200

    Add: Back-end integration to dashboard, type fixes, custom table changes and fallback

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,50 +1,49 @@
 import { Container, Flex, Text } from '@mantine/core'
 import { useCallback, useMemo, useState } from 'react'
 import { useDisclosure } from '@mantine/hooks'
+import { useApi } from 'lib/api'
 import CustomTable from 'components/common/CustomTable'
-import { BadgeType, ICampaignData } from 'types'
+import { BadgeType, ICampaign } from 'types'
+import { campaignPeriodParser } from 'utils'
+import { campaignHeaders } from 'constant'
 import { CampaignDetailsModal } from 'components/common/Modals'
 import { useNavigate } from 'react-router-dom'
 import BadgeStatusCampaign from './BadgeStatusCampaign'
-import { dashboardTableElements } from './mockData'
-
-const headings = [
-  'Campaign name',
-  'Model',
-  'Status',
-  'Served',
-  'Budget',
-  'Impressions',
-  'Clicks',
-  'CTR',
-  'Period'
-]
 
 const Dashboard = () => {
   const [opened, { open, close }] = useDisclosure(false)
-  const [selectedItem, setSelectedItem] = useState<ICampaignData | null>(null)
+  const [selectedItem, setSelectedItem] = useState<ICampaign | null>(null)
   const navigate = useNavigate()
+  const [campaignData, isResolved, error] = useApi<ICampaign[]>({
+    // we can also make an independent file handling all the endpoints and calling a function
+    // ex. const [selectedItem, setSelectedItem] = getCampaignsByOwner() and hold all the requests there
+    // in case of endpoint change we can modify only in one place and affect all dependent files
+    endpoint: '/dsp/campaigns/by-owner'
+  })
+
   const elements = useMemo(
     () =>
-      dashboardTableElements.map((el) => {
-        return {
-          id: el.id,
-          campaignName: el.campaignName,
-          model: el.model,
-          status: <BadgeStatusCampaign type={el.status as BadgeType} />,
-          served: el.served,
-          budget: el.budget,
-          impressions: el.impressions.toLocaleString(),
-          clicks: el.clicks.toLocaleString(),
-          ctr: el.ctr,
-          period: el.period
-        }
-      }),
-    []
+      campaignData
+        ? campaignData?.map((el: ICampaign) => {
+            return {
+              id: el.id,
+              title: el.title,
+              model: el.type,
+              status: <BadgeStatusCampaign type={el.status as BadgeType} />,
+              served: 'No data',
+              budget: 'No data',
+              impressions: 'No data',
+              clicks: 'No data',
+              ctr: 'No data',
+              period: campaignPeriodParser([el.activeFrom, el.activeTo])
+            }
+          })
+        : [],
+    [campaignData]
   )
 
   const handlePreview = useCallback(
-    (item: ICampaignData) => {
+    (item: ICampaign) => {
       setSelectedItem(item)
       open()
     },
@@ -52,17 +51,17 @@ const Dashboard = () => {
   )
 
   const handleAnalytics = useCallback(
-    (item: ICampaignData) => {
+    (item: ICampaign) => {
       navigate(`/campaign-analytics/${item.id}`)
     },
     [navigate]
   )
 
-  const handleDuplicate = useCallback((item: ICampaignData) => {
+  const handleDuplicate = useCallback((item: ICampaign) => {
     console.log('item', item)
   }, [])
 
-  const handleDelete = useCallback((item: ICampaignData) => {
+  const handleDelete = useCallback((item: ICampaign) => {
     console.log('item', item)
   }, [])
 
@@ -72,15 +71,20 @@ const Dashboard = () => {
         <Text size="sm" color="secondaryText" weight="bold" mb="md">
           All Campaigns
         </Text>
-        <CustomTable
-          background
-          headings={headings}
-          elements={elements}
-          onPreview={handlePreview}
-          onAnalytics={handleAnalytics}
-          onDuplicate={handleDuplicate}
-          onDelete={handleDelete}
-        />
+        {!isResolved && 'Loading'}
+        {error ? (
+          'Error getting data'
+        ) : (
+          <CustomTable
+            background
+            headings={campaignHeaders}
+            elements={elements}
+            onPreview={handlePreview}
+            onAnalytics={handleAnalytics}
+            onDuplicate={handleDuplicate}
+            onDelete={handleDelete}
+          />
+        )}
       </Flex>
       <CampaignDetailsModal item={selectedItem} opened={opened} close={close} />
     </Container>
