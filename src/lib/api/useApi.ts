f4eeb8638ede3987c7aec31b50c0ef416e698754
Kristiyan Stefanov <46137735+KristiyanStefanov23@users.noreply.github.com>
2024-03-20 16:49:22 +0200

    Fix: useAuth hook to carry auth bearer token

--- src/lib/api/useApi.ts
+++ src/lib/api/useApi.ts
@@ -1,3 +1,4 @@
+import useAccount from 'hooks/useAccount'
 import { useEffect, useState } from 'react'
 
 // TODO: add link to actual deploy. Check if in dev env to use localhost
@@ -17,6 +18,7 @@ function useApi<T>(options: RequestOptions): UseApi<T> {
   const [data, setData] = useState<T | null>(null)
   const [fulfilled, setFulfilled] = useState(false)
   const [error, setError] = useState(false)
+  const { adexAccount } = useAccount()
 
   useEffect(() => {
     const { endpoint, method = 'GET', headers = {}, queryParams, body } = options
@@ -28,14 +30,19 @@ function useApi<T>(options: RequestOptions): UseApi<T> {
     fetch(fullUrl, {
       method,
       headers: {
+        'X-DSP-Auth': `Bearer ${adexAccount?.accessToken}`,
         ...headers
       },
       body: JSON.stringify(body)
     })
-      .then((resp) => resp.json().then(setData))
+      .then((resp) => {
+        setError(!resp.ok)
+        if (resp.status === 401) console.log('Check Auth')
+        resp.json().then(setData)
+      })
       .catch(() => setError(true))
       .finally(() => setFulfilled(true))
-  }, [options])
+  }, [])
 
   return [data, fulfilled, error]
 }
