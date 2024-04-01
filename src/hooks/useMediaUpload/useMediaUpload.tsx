8fd9fc19e3df7221d77755e3b8f3ef9662a3c507
IvoPaunov <ivo.paunov@gmail.com>
2024-04-01 17:50:59 +0300

    add types to media upload

--- src/hooks/useMediaUpload/useMediaUpload.tsx
+++ src/hooks/useMediaUpload/useMediaUpload.tsx
@@ -1,16 +1,27 @@
 import { useAdExApi } from 'hooks/useAdexServices'
 import { useCallback } from 'react'
 
+type MediaUploadRes = {
+  ipfsUrl: string
+  webUrl: string
+  isPinned: boolean
+}
+
+type ZipUploadRes = {
+  root: MediaUploadRes
+  ipfsUrls: MediaUploadRes[]
+}
+
 const useMediaUpload = () => {
   const { adexServicesRequest } = useAdExApi()
 
   const uploadMedia = useCallback(
-    async (media: Blob, mediaName: string, shouldPin: boolean = false) => {
+    async (media: Blob, mediaName: string, shouldPin: boolean = false): Promise<MediaUploadRes> => {
       const formData = new FormData()
       formData.append('media', media, mediaName)
       formData.append('shouldPin', shouldPin.toString())
 
-      return adexServicesRequest('backend', {
+      return adexServicesRequest<MediaUploadRes>('backend', {
         route: '/dsp/ipfs/upload',
         method: 'POST',
         body: formData
@@ -20,12 +31,12 @@ const useMediaUpload = () => {
   )
 
   const uploadZipMedia = useCallback(
-    async (media: Blob, mediaName: string, shouldPin: boolean = false) => {
+    async (media: Blob, mediaName: string, shouldPin: boolean = false): Promise<ZipUploadRes> => {
       const formData = new FormData()
       formData.append('zip', media, mediaName)
       formData.append('shouldPin', shouldPin.toString())
 
-      return adexServicesRequest('backend', {
+      return adexServicesRequest<ZipUploadRes>('backend', {
         route: '/dsp/ipfs/upload-zip',
         method: 'POST',
         body: formData
