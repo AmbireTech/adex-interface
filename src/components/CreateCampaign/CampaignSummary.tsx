e7cd3b7b33e4e297d5e7e25d096b1bffb92a342e
Lyubomir Nanev <lyubomir.nanev@gmail.com>
2024-04-08 14:55:26 +0300

    Add: ConfirmModal component. WIP

--- src/components/CreateCampaign/CampaignSummary.tsx
+++ src/components/CreateCampaign/CampaignSummary.tsx
@@ -6,6 +6,8 @@ import LeftArrowIcon from 'resources/icons/LeftArrow'
 import { useCreateCampaignFormContext } from 'contexts/CreateCampaignFormContext'
 import useCreateCampaignData from 'hooks/useCreateCampaignData/useCreateCampaignData'
 import CampaignDetailsRow from 'components/common/CampainDetailsRow'
+import ConfirmModal from 'components/common/Modals/ConfirmModal'
+import AttentionIcon from 'resources/icons/Attention'
 
 const useStyles = createStyles((theme) => ({
   bg: {
@@ -21,6 +23,28 @@ const useStyles = createStyles((theme) => ({
   },
   brandColor: {
     color: theme.colors.brand[theme.fn.primaryShade()]
+  },
+  confirmModalContent: {
+    background:
+      theme.colors.attention[theme.fn.primaryShade()] + theme.other.shades.hexColorSuffix.lightest,
+    padding: theme.spacing.md,
+    border: `1px solid ${
+      theme.colors.attention[theme.fn.primaryShade()] + theme.other.shades.hexColorSuffix.lighter
+    }`,
+    borderRadius: theme.spacing.sm
+  },
+  attentionIcon: {
+    width: 25,
+    height: 25,
+    color: theme.colors.attention[theme.fn.primaryShade()]
+  },
+  iconWrapper: {
+    width: 50,
+    height: 50,
+    // TODO: Add the suffix 1A to the theme
+    background: `${theme.colors.attention[theme.fn.primaryShade()]}1A`,
+    borderRadius: '50%',
+    padding: theme.spacing.sm
   }
 }))
 
@@ -55,20 +79,23 @@ const CampaignSummary = () => {
   const isTheLastStep = useMemo(() => step === CREATE_CAMPAIGN_STEPS - 1, [step])
   const isFirstStep = useMemo(() => step === 0, [step])
   const launchCampaign = useCallback(() => {
+    // TODO:
+    // Open Confirm modal here. On confirm publish Campaign
     publishCampaign()
       .then((res) => {
-        // TODO: Add Congrats modal here
+        // Congrats modal here
+        // Clear the Campaign object
         console.log('response', res)
       })
       .catch((error) => {
         // TOOD: handle the error
         console.log('error', error.message)
       })
-    // TODO: REVOKE all the blob URLs
-    // URL.revokeObjectURL(storedImageURL);
     console.log('LAUNCH CAMPAIGN')
   }, [publishCampaign])
+
   const form = useCreateCampaignFormContext()
+
   const handleNextStepBtnClicked = useCallback(() => {
     if (step < CREATE_CAMPAIGN_STEPS - 1) {
       if (step === 2) {
@@ -80,13 +107,8 @@ const CampaignSummary = () => {
       }
 
       updateCampaign('step', step + 1)
-      return
     }
-
-    if (isTheLastStep) {
-      launchCampaign()
-    }
-  }, [isTheLastStep, step, updateCampaign, form, launchCampaign])
+  }, [step, updateCampaign, form])
 
   return (
     <>
@@ -121,16 +143,41 @@ const CampaignSummary = () => {
         <Text color="secondaryText">0</Text>
       </Flex>
       <Flex direction="column" justify="space-between" align="center">
-        <Button
-          w="90%"
-          disabled={isNextBtnDisabled}
-          size="lg"
-          mt="md"
-          variant="filled"
-          onClick={handleNextStepBtnClicked}
-        >
-          {isTheLastStep ? 'Launch Campaign' : 'Next Step'}
-        </Button>
+        {!isTheLastStep ? (
+          <Button
+            w="90%"
+            disabled={isNextBtnDisabled}
+            size="lg"
+            mt="md"
+            variant="filled"
+            onClick={handleNextStepBtnClicked}
+          >
+            Next Step
+          </Button>
+        ) : (
+          <ConfirmModal
+            w="90%"
+            size="lg"
+            mt="md"
+            variant="filled"
+            title="Launch Campaign"
+            btnLabel="Launch Campaign"
+            cancelBtnLabel="Go Back"
+            confirmBtnLabel="Launch Campaign"
+            onCancelClicked={() => console.log('Canceled')}
+            onConfirmClicked={launchCampaign}
+          >
+            <Flex justify="center" className={classes.confirmModalContent}>
+              <div className={classes.iconWrapper}>
+                <AttentionIcon className={classes.attentionIcon} />
+              </div>
+              <Text align="center">
+                Once you click on “Launch campaign,” any further edits to the campaign will be
+                disabled. Are you certain you wish to proceed with the launch?
+              </Text>
+            </Flex>
+          </ConfirmModal>
+        )}
         <Button w="90%" size="lg" mt="md" variant="outline">
           Save Draft
         </Button>
