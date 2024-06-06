8fb4aa76c089924586ebe87625c0d18a6165329a
Lyubomir Nanev <lyubomir.nanev@gmail.com>
2024-06-06 14:49:22 +0300

    Changed the design of step one. WIP

--- src/components/CreateCampaign/StepOne/BannerSizesList.tsx
+++ src/components/CreateCampaign/StepOne/BannerSizesList.tsx
@@ -1,21 +1,22 @@
-import { Grid } from '@mantine/core'
-import BannerSizeMock from 'components/common/BannerSizeMock'
+import { Alert } from '@mantine/core'
+// import BannerSizeMock from 'components/common/BannerSizeMock'
 import { checkBannerSizes } from 'helpers/createCampaignHelpers'
 import useCreateCampaignContext from 'hooks/useCreateCampaignContext'
 import { useMemo } from 'react'
 import { AdUnit } from 'adex-common/dist/types'
-import useCreateCampaignData from 'hooks/useCreateCampaignData/useCreateCampaignData'
+// import useCreateCampaignData from 'hooks/useCreateCampaignData/useCreateCampaignData'
 import { SupplyStatsDetails } from 'types'
+import InfoIcon from 'resources/icons/Info'
 
 const getPopularBannerSizes = (bannerSizes: SupplyStatsDetails[] | SupplyStatsDetails[][]) => {
   let result: SupplyStatsDetails[][] | SupplyStatsDetails[] = []
 
   if (bannerSizes.length && Array.isArray(bannerSizes[0])) {
     result = (bannerSizes as SupplyStatsDetails[][])
-      .map((item: SupplyStatsDetails[]) => item.slice(0, 10))
+      .map((item: SupplyStatsDetails[]) => item.slice(0, 6))
       .flat()
   } else {
-    result = (bannerSizes as SupplyStatsDetails[]).slice(0, 10)
+    result = (bannerSizes as SupplyStatsDetails[]).slice(0, 6)
   }
 
   return result.sort((a, b) => b.count - a.count)
@@ -23,7 +24,7 @@ const getPopularBannerSizes = (bannerSizes: SupplyStatsDetails[] | SupplyStatsDe
 
 const BannerSizesList = ({ adUnits }: { adUnits: AdUnit[] }) => {
   const { selectedBannerSizes } = useCreateCampaignContext()
-  const { uniqueSizesWithCount } = useCreateCampaignData()
+  // const { uniqueSizesWithCount } = useCreateCampaignData()
 
   const popularBannerSizes = useMemo(
     () =>
@@ -42,23 +43,26 @@ const BannerSizesList = ({ adUnits }: { adUnits: AdUnit[] }) => {
   )
 
   return updatedBannerSizes ? (
-    <Grid columns={10}>
-      {updatedBannerSizes.map((item) => {
-        const addedBannerCount = uniqueSizesWithCount.find(
-          ({ value }) => item.value === value
-        )?.count
+    // <Grid columns={10}>
+    //   {updatedBannerSizes.map((item) => {
+    //     const addedBannerCount = uniqueSizesWithCount.find(
+    //       ({ value }) => item.value === value
+    //     )?.count
 
-        return (
-          <Grid.Col xs={2} sm={2} md={2} lg={2} xl={1} key={`${item.value}+${item.count}`}>
-            <BannerSizeMock
-              variant={item.value}
-              active={!!item.checked}
-              addedBannerCount={addedBannerCount}
-            />
-          </Grid.Col>
-        )
-      })}
-    </Grid>
+    //     return (
+    //       <Grid.Col xs={2} sm={2} md={2} lg={2} xl={1} key={`${item.value}+${item.count}`}>
+    //         <BannerSizeMock
+    //           variant={item.value}
+    //           active={!!item.checked}
+    //           addedBannerCount={addedBannerCount}
+    //         />
+    //       </Grid.Col>
+    //     )
+    //   })}
+    // </Grid>
+    <Alert icon={<InfoIcon style={{ marginTop: 0 }} />} color="attention" variant="outline">
+      Recommended banner sizes: {updatedBannerSizes.map((size) => size.value).join(', ')}
+    </Alert>
   ) : null
 }
 
