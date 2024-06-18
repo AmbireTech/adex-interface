8e6375df5fe1a6542148ae9389c4886c973d678f
Lyubomir Nanev <lyubomir.nanev@gmail.com>
2024-06-18 13:11:47 +0300

    Add: Confirm modal on top up account

--- src/components/SideNav/NavLink.tsx
+++ src/components/SideNav/NavLink.tsx
@@ -1,48 +1,67 @@
-import { UnstyledButton, Group, Text, createStyles } from '@mantine/core'
+import { useCallback, useState } from 'react'
+import { UnstyledButton, Group, Text, createStyles, Flex, Modal, Button } from '@mantine/core'
 import { Link, LinkProps } from 'react-router-dom'
-import CustomPopover from 'components/common/CustomPopover'
+import AttentionIcon from 'resources/icons/Attention'
 
-const useStyles = createStyles((theme) => {
-  return {
-    button: {
-      display: 'block',
-      width: '100%',
-      height: '100%',
-      padding: theme.spacing.xs,
-      borderRadius: 'none',
-      position: 'relative',
-      color: theme.fn.lighten(
-        theme.colors.mainText[theme.fn.primaryShade()],
-        theme.other.shades.lighten.lighter
-      ),
-      '&:hover': {
-        background: theme.colors.lightBackground[theme.fn.primaryShade()]
-      }
-    },
-    active: {
-      // backgroundColor: theme.fn.primaryColor() + theme.other.shades.hexColorSuffix.lightest,
-      color: theme.fn.primaryColor(),
-      fontWeight: theme.other.fontWeights.regular,
-      opacity: 1,
-      '&:before': {
-        content: '""',
-        position: 'absolute',
-        left: 0,
-        top: 0,
-        bottom: 0,
-        width: 5,
-        backgroundColor: theme.fn.primaryColor()
-      }
-    },
-    icon: {
-      height: 26,
-      width: 26,
-      display: 'flex',
-      alignItems: 'center',
-      marginRight: theme.spacing.xs
+const useStyles = createStyles((theme) => ({
+  button: {
+    display: 'block',
+    width: '100%',
+    height: '100%',
+    padding: theme.spacing.xs,
+    borderRadius: 'none',
+    position: 'relative',
+    color: theme.fn.lighten(
+      theme.colors.mainText[theme.fn.primaryShade()],
+      theme.other.shades.lighten.lighter
+    ),
+    '&:hover': {
+      background: theme.colors.lightBackground[theme.fn.primaryShade()]
     }
+  },
+  active: {
+    // backgroundColor: theme.fn.primaryColor() + theme.other.shades.hexColorSuffix.lightest,
+    color: theme.fn.primaryColor(),
+    fontWeight: theme.other.fontWeights.regular,
+    opacity: 1,
+    '&:before': {
+      content: '""',
+      position: 'absolute',
+      left: 0,
+      top: 0,
+      bottom: 0,
+      width: 5,
+      backgroundColor: theme.fn.primaryColor()
+    }
+  },
+  icon: {
+    height: 26,
+    width: 26,
+    display: 'flex',
+    alignItems: 'center',
+    marginRight: theme.spacing.xs
+  },
+  confirmModalContent: {
+    background:
+      theme.colors.attention[theme.fn.primaryShade()] + theme.other.shades.hexColorSuffix.lightest,
+    padding: theme.spacing.xl
+  },
+  iconWrapper: {
+    width: 50,
+    height: 50,
+    background: `${theme.colors.attention[theme.fn.primaryShade()]}1A`,
+    borderRadius: '50%',
+    padding: theme.spacing.sm
+  },
+  attentionIcon: {
+    width: 25,
+    height: 25,
+    color: theme.colors.attention[theme.fn.primaryShade()]
+  },
+  root: {
+    padding: 0
   }
-})
+}))
 
 type NavLinkProps = LinkProps & {
   to?: string
@@ -50,30 +69,58 @@ type NavLinkProps = LinkProps & {
   label: string
   action?: () => void
   active?: boolean
-  hasPopover?: boolean
-  popoverContent?: JSX.Element | string
+  hasConfirmModal?: boolean
 }
 
-function NavLink({
-  to = '',
-  icon,
-  label,
-  action,
-  active,
-  hasPopover,
-  popoverContent = '',
-  ...rest
-}: NavLinkProps) {
+function NavLink({ to = '', icon, label, action, active, hasConfirmModal, ...rest }: NavLinkProps) {
   const { classes, cx } = useStyles()
-  return hasPopover ? (
-    <CustomPopover popoverContent={popoverContent}>
-      <UnstyledButton className={classes.button} px="xl">
+  const [opened, setOpened] = useState(false)
+
+  const handleModalClicked = useCallback(() => setOpened((prev) => !prev), [])
+
+  return hasConfirmModal ? (
+    <>
+      <UnstyledButton className={classes.button} onClick={handleModalClicked} px="xl">
         <Group>
           <span className={classes.icon}>{icon}</span>
           <Text size="md">{label}</Text>
         </Group>
       </UnstyledButton>
-    </CustomPopover>
+      <Modal
+        opened={opened}
+        onClose={handleModalClicked}
+        withCloseButton={false}
+        classNames={{
+          body: classes.root
+        }}
+      >
+        <Flex justify="center" className={classes.confirmModalContent}>
+          <div className={classes.iconWrapper}>
+            <AttentionIcon className={classes.attentionIcon} />
+          </div>
+          <Text>
+            To add funds to your account, please contact us at{' '}
+            <a href="mailto: contactus@adex.network"> contactus@adex.network</a> and send us you
+            account ID (the address which you can find in the upper right corner of your profile).
+            We will provide you with further instructions on how to deposit funds on the address.
+          </Text>
+        </Flex>
+        <Flex justify="space-between" p="xl">
+          <Button size="lg" variant="outline" onClick={handleModalClicked}>
+            Go back
+          </Button>
+          <Button
+            size="lg"
+            onClick={() => {
+              window.location.href = 'mailto:contactus@adex.network'
+              handleModalClicked()
+            }}
+          >
+            Contact us
+          </Button>
+        </Flex>
+      </Modal>
+    </>
   ) : (
     <UnstyledButton
       to={to}
