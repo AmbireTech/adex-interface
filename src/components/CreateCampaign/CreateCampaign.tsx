344da7f1929a9eb320f8040b4bed8fb37e8e89c0
Lyubomir Nanev <lyubomir.nanev@gmail.com>
2024-06-27 17:17:12 +0300

    Fix: unsaved changes on leaving createCampaign component

--- src/components/CreateCampaign/CreateCampaign.tsx
+++ src/components/CreateCampaign/CreateCampaign.tsx
@@ -1,10 +1,16 @@
 import { Grid, createStyles, Text, Flex, Button } from '@mantine/core'
 import useCreateCampaignContext from 'hooks/useCreateCampaignContext'
-import { useEffect } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { modals } from '@mantine/modals'
 import useCustomNotifications from 'hooks/useCustomNotifications'
 import { deepEqual } from 'helpers/createCampaignHelpers'
 import AttentionIcon from 'resources/icons/Attention'
+import type {
+  unstable_Blocker as Blocker,
+  unstable_BlockerFunction as BlockerFunction
+} from 'react-router-dom'
+import { unstable_useBlocker as useBlocker } from 'react-router-dom'
+import { CampaignUI } from 'types'
 import CustomStepper from './CampaignStepper'
 import CampaignSummary from './CampaignSummary'
 import StepOne from './StepOne/StepOne'
@@ -88,81 +94,108 @@ const Wizard = ({ step }: { step: number }) => {
 const CreateCampaign = () => {
   const { classes } = useStyles()
   const {
-    setCampaign,
+    campaign,
     campaign: { step },
-    saveToDraftCampaign,
-    defaultValue
+    saveToDraftCampaign
   } = useCreateCampaignContext()
   const { showNotification } = useCustomNotifications()
+  const [campaignInitialState, setCampaignInitialState] = useState<CampaignUI | null>(null)
+
+  const blocker: Blocker = useBlocker(
+    useCallback<BlockerFunction>(
+      ({ currentLocation, nextLocation }) => currentLocation.pathname !== nextLocation.pathname,
+      []
+    )
+  )
+
+  const blockerProceed = useCallback(() => blocker.proceed?.(), [blocker])
+
+  const handleConfirmBtnClicked = useCallback(
+    async (prevState: CampaignUI) => {
+      try {
+        const res = await saveToDraftCampaign(prevState)
+
+        if (res && res.success) {
+          showNotification('info', 'Draft saved')
+        } else {
+          showNotification('warning', 'invalid campaign data response', 'Data error')
+        }
+      } catch (err) {
+        console.error(err)
+        showNotification('error', 'Creating campaign failed', 'Data error')
+      }
+      modals.closeAll()
+      blockerProceed()
+    },
+    [showNotification, saveToDraftCampaign, blockerProceed]
+  )
+
+  const saveToDraftOnUnboundComponent = useCallback(() => {
+    modals.open({
+      withCloseButton: false,
+      closeOnClickOutside: false,
+      children: (
+        <>
+          <Flex justify="center" className={classes.confirmModalContent}>
+            <div className={classes.iconWrapper}>
+              <AttentionIcon className={classes.attentionIcon} />
+            </div>
+            <Text w="100%">You may have unsaved changes. Do you want to save them as a draft?</Text>
+          </Flex>
+          <Flex justify="space-between" p="xl">
+            <Button
+              size="lg"
+              variant="outline"
+              onClick={() => {
+                modals.closeAll()
+                if (blocker.state === 'blocked') blockerProceed()
+
+                console.log('No')
+              }}
+            >
+              No
+            </Button>
+            <Button size="lg" onClick={() => handleConfirmBtnClicked(campaign)}>
+              Yes
+            </Button>
+          </Flex>
+        </>
+      ),
+      classNames: {
+        body: classes.modalBody
+      }
+    })
+  }, [
+    campaign,
+    classes.attentionIcon,
+    classes.confirmModalContent,
+    classes.iconWrapper,
+    classes.modalBody,
+    handleConfirmBtnClicked,
+    blocker,
+    blockerProceed
+  ])
 
   useEffect(() => {
-    return () => {
-      // NOTE: because of the strict mode on dev env it invokes twice
-      if (process.env.NODE_ENV !== 'development') {
-        setCampaign((prev) => {
-          if (!deepEqual(prev, defaultValue)) {
-            modals.open({
-              withCloseButton: false,
-              closeOnClickOutside: false,
-              children: (
-                <>
-                  <Flex justify="center" className={classes.confirmModalContent}>
-                    <div className={classes.iconWrapper}>
-                      <AttentionIcon className={classes.attentionIcon} />
-                    </div>
-                    <Text w="100%">
-                      You may have unsaved changes. Do you want to save them as a draft?
-                    </Text>
-                  </Flex>
-                  <Flex justify="space-between" p="xl">
-                    <Button
-                      size="lg"
-                      variant="outline"
-                      onClick={() => {
-                        modals.closeAll()
-                        console.log('No')
-                      }}
-                    >
-                      No
-                    </Button>
-                    <Button
-                      size="lg"
-                      onClick={async () => {
-                        try {
-                          const res = await saveToDraftCampaign(prev)
-
-                          if (res && res.success) {
-                            // TODO: move this func out of the button @boklik 🤷🏼‍♂️
-                            showNotification('error', 'Draft saved')
-                          } else {
-                            showNotification(
-                              'warning',
-                              'invalid campaign data response',
-                              'Data error'
-                            )
-                          }
-                        } catch (err) {
-                          console.error(err)
-                          showNotification('error', 'Creating campaign failed', 'Data error')
-                        }
-                        modals.closeAll()
-                      }}
-                    >
-                      Yes
-                    </Button>
-                  </Flex>
-                </>
-              ),
-              classNames: {
-                body: classes.modalBody
-              }
-            })
-          }
-
-          return prev
-        })
-      }
+    if (
+      blocker.state === 'blocked' &&
+      !!campaignInitialState &&
+      !deepEqual(campaign, campaignInitialState)
+    ) {
+      saveToDraftOnUnboundComponent()
+    } else if (blocker.state === 'blocked') {
+      blockerProceed()
     }
+  }, [saveToDraftOnUnboundComponent, blocker, campaignInitialState, campaign, blockerProceed])
+
+  useEffect(() => {
+    setCampaignInitialState((p) => {
+      let next = p && { ...p }
+      if (!next) {
+        next = campaign
+      }
+      return next
+    })
   }, []) // eslint-disable-line
 
   return (
