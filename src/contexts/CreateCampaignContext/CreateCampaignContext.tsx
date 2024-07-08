7457f6d70d04434d3fc20cc8d2cf31b3b417a1eb
IvoPaunov <ivo.paunov@gmail.com>
2024-07-08 15:44:59 +0300

    fix create campaign default start time

--- src/contexts/CreateCampaignContext/CreateCampaignContext.tsx
+++ src/contexts/CreateCampaignContext/CreateCampaignContext.tsx
@@ -157,7 +157,7 @@ const CreateCampaignContextProvider: FC<PropsWithChildren> = ({ children }) => {
       outpaceAssetDecimals: balanceToken.decimals,
       outpaceChainId: balanceToken.chainId,
       startsAt: isPastDateTime(CREATE_CAMPAIGN_DEFAULT_VALUE.startsAt)
-        ? dayjs().add(1, 'hour').toDate()
+        ? dayjs().add(1, 'minute').toDate()
         : CREATE_CAMPAIGN_DEFAULT_VALUE.startsAt
     }),
     [adexAccount?.address, balanceToken?.address, balanceToken?.decimals, balanceToken?.chainId]
