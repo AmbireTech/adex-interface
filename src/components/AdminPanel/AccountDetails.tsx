3a9caea5f9e4603167beeddb908d185bca760b83
IvoPaunov <ivo.paunov@gmail.com>
2024-07-11 13:20:29 +0300

    wip: account details

--- src/components/AdminPanel/AccountDetails.tsx
+++ src/components/AdminPanel/AccountDetails.tsx
@@ -1,9 +1,10 @@
 import { useCallback, useMemo, useState } from 'react'
 import { useForm, isInRange, hasLength, matches } from '@mantine/form'
-import { Button, Group, TextInput, Box, NumberInput } from '@mantine/core'
+import { Button, Group, TextInput, Box, NumberInput, Grid, Accordion } from '@mantine/core'
 import useAdmin from 'hooks/useAdmin'
 import throttle from 'lodash.throttle'
 import { useParams } from 'react-router-dom'
+import { FundsActivity } from './AccountDetailsElements'
 
 type Deposit = {
   accountId: string
@@ -18,15 +19,16 @@ type Deposit = {
 }
 
 function AccountDetails() {
-  const { accountId } = useParams()
-  const { makeDeposit } = useAdmin()
+  const { accountId = '' } = useParams()
+  const { makeDeposit, accounts, initialDataLoading } = useAdmin()
   const [loading, setLoading] = useState(false)
+  const accountData = useMemo(() => accounts.get(accountId), [accounts, accountId])
 
   console.log({ accountId })
 
   const form = useForm<Deposit>({
     initialValues: {
-      accountId: accountId || '',
+      accountId,
       amount: 0,
       token: {
         name: 'USDC',
@@ -68,70 +70,89 @@ function AccountDetails() {
     return throttle(handleSubmit, 3000, { leading: true })
   }, [handleSubmit])
 
-  return (
-    <Box component="form" maw={400} mx="auto" onSubmit={form.onSubmit(throttledSbm)}>
-      <TextInput
-        label="Account address"
-        placeholder="0x000"
-        withAsterisk
-        {...form.getInputProps('accountId')}
-        disabled
-      />
-      <NumberInput
-        mt="sm"
-        label="Amount"
-        type="number"
-        placeholder="Amount"
-        hideControls
-        min={0}
-        {...form.getInputProps('amount')}
-      />
-      <TextInput
-        label="Token name"
-        placeholder="Token name"
-        withAsterisk
-        disabled
-        mt="md"
-        {...form.getInputProps('token.name')}
-      />
-      <TextInput
-        label="Token addr"
-        placeholder="Token addr"
-        withAsterisk
-        disabled
-        mt="md"
-        {...form.getInputProps('token.address')}
-      />
-      <TextInput
-        label="Token chain id"
-        placeholder="Token chain id"
-        withAsterisk
-        disabled
-        mt="md"
-        {...form.getInputProps('token.chainId')}
-      />
-      <NumberInput
-        label="Token decimals"
-        placeholder="token decimals"
-        withAsterisk
-        disabled
-        mt="md"
-        {...form.getInputProps('token.decimals')}
-      />
-      <TextInput
-        label="Tx hash"
-        placeholder="0x0000000000000000000000000000000000000000000000000000000000000000"
-        withAsterisk
-        mt="md"
-        {...form.getInputProps('txHash')}
-      />
+  if (!accountData || initialDataLoading) {
+    return <div>{`Invalid account id ${accountId}`}</div>
+  }
 
-      <Group position="right" mt="md">
-        <Button type="submit" disabled={loading}>
-          Submit
-        </Button>
-      </Group>
-    </Box>
+  return (
+    <Grid>
+      <Grid.Col span={6}>{accountData.info?.contactPerson}</Grid.Col>
+      <Grid.Col span={6}>
+        <Accordion defaultValue="deposit-form">
+          <Accordion.Item value="deposit-form">
+            <Accordion.Control>Deposit form</Accordion.Control>
+            <Accordion.Panel>
+              <Box component="form" maw={400} mx="auto" onSubmit={form.onSubmit(throttledSbm)}>
+                <TextInput
+                  label="Account address"
+                  placeholder="0x000"
+                  withAsterisk
+                  {...form.getInputProps('accountId')}
+                  disabled
+                />
+                <NumberInput
+                  mt="sm"
+                  label="Amount"
+                  type="number"
+                  placeholder="Amount"
+                  hideControls
+                  min={0}
+                  {...form.getInputProps('amount')}
+                />
+                <TextInput
+                  label="Token name"
+                  placeholder="Token name"
+                  withAsterisk
+                  disabled
+                  mt="md"
+                  {...form.getInputProps('token.name')}
+                />
+                <TextInput
+                  label="Token addr"
+                  placeholder="Token addr"
+                  withAsterisk
+                  disabled
+                  mt="md"
+                  {...form.getInputProps('token.address')}
+                />
+                <TextInput
+                  label="Token chain id"
+                  placeholder="Token chain id"
+                  withAsterisk
+                  disabled
+                  mt="md"
+                  {...form.getInputProps('token.chainId')}
+                />
+                <NumberInput
+                  label="Token decimals"
+                  placeholder="token decimals"
+                  withAsterisk
+                  disabled
+                  mt="md"
+                  {...form.getInputProps('token.decimals')}
+                />
+                <TextInput
+                  label="Tx hash"
+                  placeholder="0x0000000000000000000000000000000000000000000000000000000000000000"
+                  withAsterisk
+                  mt="md"
+                  {...form.getInputProps('txHash')}
+                />
+
+                <Group position="right" mt="md">
+                  <Button type="submit" disabled={loading}>
+                    Submit
+                  </Button>
+                </Group>
+              </Box>
+            </Accordion.Panel>
+          </Accordion.Item>
+        </Accordion>
+      </Grid.Col>
+      <Grid.Col span={12}>
+        <FundsActivity accountData={accountData} />
+      </Grid.Col>
+    </Grid>
   )
 }
 
