0dc37bf8ed112c4447d3441f4986323e08bcce60
IvoPaunov <ivo.paunov@gmail.com>
2024-07-19 10:47:32 +0300

    add campaigns do admin account details

--- src/components/AdminPanel/AdminPanel.tsx
+++ src/components/AdminPanel/AdminPanel.tsx
@@ -1,4 +1,4 @@
-import { Tabs } from '@mantine/core'
+import { Tabs, Paper } from '@mantine/core'
 import { useNavigate, useParams, Outlet } from 'react-router-dom'
 import { AdminBadge } from 'components/common/AdminBadge'
 import Dashboard from 'components/Dashboard'
@@ -9,7 +9,7 @@ import { SspStats } from './SspStats'
 
 const AdminPanel = () => {
   const navigate = useNavigate()
-  const { tabValue = 'campaigns' } = useParams()
+  const { tabValue = 'campaigns', accountId } = useParams()
 
   console.log({ tabValue })
 
@@ -23,7 +23,7 @@ const AdminPanel = () => {
         keepMounted={false}
       >
         <Tabs.List>
-          <Tabs.Tab value="campaigns">Campaigns</Tabs.Tab>
+          <Tabs.Tab value="campaigns">All Campaigns</Tabs.Tab>
           <Tabs.Tab value="validatorAnalytics">Validator Analytics</Tabs.Tab>
           <Tabs.Tab value="sspStats">SSP stats</Tabs.Tab>
           <Tabs.Tab value="accounts">Accounts</Tabs.Tab>
@@ -33,7 +33,9 @@ const AdminPanel = () => {
         </Tabs.List>
 
         <Tabs.Panel value="campaigns" pt="xs">
-          <Dashboard isAdminPanel />
+          <Paper p="sm" withBorder>
+            <Dashboard isAdminPanel accountId={accountId} />
+          </Paper>
         </Tabs.Panel>
 
         <Tabs.Panel value="validatorAnalytics" pt="xs">
