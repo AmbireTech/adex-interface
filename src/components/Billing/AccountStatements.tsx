5d629ad4288839c5a0f53ce1f1fa561bb30ba6b9
IvoPaunov <ivo.paunov@gmail.com>
2024-09-10 11:38:14 +0300

    cleanup statement when no data

--- src/components/Billing/AccountStatements.tsx
+++ src/components/Billing/AccountStatements.tsx
@@ -1,5 +1,4 @@
 import { useMemo, useCallback, useState, SetStateAction } from 'react'
-import { Title } from '@mantine/core'
 import CustomTable from 'components/common/CustomTable'
 import { useDisclosure } from '@mantine/hooks'
 import useAccount from 'hooks/useAccount'
@@ -29,6 +28,7 @@ const getTokenIndex = (token: OperationEntry['token']): string =>
 const Statements = () => {
   const [opened, { open, close }] = useDisclosure(false)
   const {
+    isLoading,
     adexAccount: { address, billingDetails, fundsDeposited, fundsOnCampaigns, refundsFromCampaigns }
   } = useAccount()
 
@@ -135,10 +135,6 @@ const Statements = () => {
     ]
   }, [onPreview])
 
-  if (!statements) {
-    return <Title order={4}>No AccountStatements found.</Title>
-  }
-
   return (
     <>
       <BillingDetailsModal title="Statement" loading={!statement} opened={opened} close={close}>
@@ -150,7 +146,13 @@ const Statements = () => {
           />
         )}
       </BillingDetailsModal>
-      <CustomTable headings={columnTitles} elements={elements} actions={actions} shadow="xs" />
+      <CustomTable
+        headings={columnTitles}
+        elements={elements}
+        actions={actions}
+        shadow="xs"
+        loading={isLoading}
+      />
     </>
   )
 }
