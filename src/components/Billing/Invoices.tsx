fc7a61387f27a8eb2f8274439bd9f561565af7f9
IvoPaunov <ivo.paunov@gmail.com>
2024-09-26 16:17:25 +0300

    wip: invoices in admin panel

--- src/components/Billing/Invoices.tsx
+++ src/components/Billing/Invoices.tsx
@@ -4,23 +4,77 @@ import { useCallback, useEffect, useMemo, useState } from 'react'
 import { CampaignStatus } from 'adex-common'
 import { useCampaignsData } from 'hooks/useCampaignsData'
 import useAccount from 'hooks/useAccount'
-import { formatDateShort } from 'helpers'
+import { formatDateShort, parseBigNumTokenAmountToDecimal } from 'helpers'
 import VisibilityIcon from 'resources/icons/Visibility'
+import useAdmin from 'hooks/useAdmin'
 import { InvoicesModal } from './InvoicesModal'
 
 const columnTitles = ['Company Name', 'Campaign', 'Campaign Period']
 
-const Invoices = () => {
+const Invoices = ({ forAdmin }: { forAdmin?: boolean }) => {
   const [opened, { open, close }] = useDisclosure(false)
-  const { campaignsData, updateAllCampaignsData, initialDataLoading } = useCampaignsData()
+  const { campaignsData, updateAllCampaignsData } = useCampaignsData()
   const campaigns = useMemo(() => Array.from(campaignsData.values()), [campaignsData])
-  const {
-    adexAccount: {
-      billingDetails: { companyName }
-    }
-  } = useAccount()
+  const { accounts, initialDataLoading, getAllAccounts } = useAdmin()
+  const { adexAccount } = useAccount()
 
   const [selectedCampaignId, setSelectedCampaignId] = useState('')
+  const campaignData = useMemo(
+    () => campaignsData.get(selectedCampaignId),
+
+    [selectedCampaignId, campaignsData]
+  )
+
+  const selectedCampaignData = useMemo(() => campaignData, [campaignData])
+  const selectedCampaign = useMemo(() => campaignData?.campaign, [campaignData?.campaign])
+
+  const account = useMemo(
+    () => (forAdmin ? accounts.get(selectedCampaign?.owner || '') || adexAccount : adexAccount),
+    [accounts, adexAccount, selectedCampaign?.owner, forAdmin]
+  )
+
+  const invoiceData = useMemo(() => {
+    const to = Number(selectedCampaign?.activeTo || 0)
+
+    const campaignOpenData = account.fundsOnCampaigns.perCampaign.find(
+      (item) => item.id === selectedCampaign?.id
+    )
+    const campaignCloseData = account.refundsFromCampaigns.perCampaign.find(
+      (item) => item.id === selectedCampaign?.id
+    )
+    const currencyName = campaignOpenData?.token.name || ''
+    const decimals = campaignOpenData?.token.decimals || 6
+
+    // NOTE: the actual payment is when the campaign is started (openings -> start dates, activeFrom is fallback)
+    // The question is: Is that ok from accounting stand point
+    // TODO: Should we have invoices for the full amount and credit notes for the refunds (on stop or expire with no full budget used)
+    const start = new Date(
+      campaignOpenData?.startDate || Number(selectedCampaign?.activeFrom) || 0
+    ).getTime()
+
+    const end = new Date(campaignCloseData?.closeDate || to).getTime()
+
+    // TODO: discuss the payment and invoice date
+
+    const amount = parseBigNumTokenAmountToDecimal(
+      BigInt(campaignOpenData?.amount || selectedCampaign?.campaignBudget || 0) -
+        BigInt(campaignCloseData?.amount || 0),
+      decimals
+    )
+    return {
+      invoiceDate: Math.min(end, to),
+      paymentDate: start,
+      amount,
+      currencyName
+    }
+  }, [
+    account.fundsOnCampaigns.perCampaign,
+    account.refundsFromCampaigns.perCampaign,
+    selectedCampaign?.activeFrom,
+    selectedCampaign?.activeTo,
+    selectedCampaign?.campaignBudget,
+    selectedCampaign?.id
+  ])
 
   const invoiceElements = useMemo(
     () =>
@@ -38,7 +92,11 @@ const Invoices = () => {
           return {
             id: campaign.campaignId,
             columns: [
-              { value: companyName },
+              {
+                value: forAdmin
+                  ? accounts.get(campaign.campaign.owner)?.billingDetails?.companyName
+                  : adexAccount.billingDetails.companyName
+              },
               { value: campaign.campaign.title },
               {
                 value: campaign.campaign.activeFrom,
@@ -53,13 +111,17 @@ const Invoices = () => {
             ]
           }
         }),
-    [campaigns, companyName]
+    [accounts, adexAccount.billingDetails.companyName, campaigns, forAdmin]
   )
 
   useEffect(() => {
     updateAllCampaignsData(true)
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
+
+  useEffect(() => {
+    getAllAccounts()
+  }, [getAllAccounts])
 
   const handlePreview = useCallback(
     (item: { id: string }) => {
@@ -89,7 +151,13 @@ const Invoices = () => {
         shadow="xs"
         loading={initialDataLoading}
       />
-      <InvoicesModal campaignId={selectedCampaignId} opened={opened} close={close} />
+      <InvoicesModal
+        campaignData={selectedCampaignData}
+        opened={opened}
+        invoiceData={invoiceData}
+        account={account}
+        close={close}
+      />
     </>
   )
 }
--- src/components/Billing/InvoicesModal.tsx
+++ src/components/Billing/InvoicesModal.tsx
@@ -1,75 +1,73 @@
 import { InvoicesPDF } from 'components/Billing/BillingPDF'
 import { ADEX_COMPANY_DETAILS } from 'constants/adexCompanyDetatils'
-import useAccount from 'hooks/useAccount'
+import { Account, CampaignData, IInvoiceDetails } from 'types'
 import { useCampaignsData } from 'hooks/useCampaignsData'
 import { useMemo } from 'react'
-import { IInvoiceDetails } from 'types'
-import { parseBigNumTokenAmountToDecimal } from 'helpers'
+// import { parseBigNumTokenAmountToDecimal } from 'helpers'
+
 import { BillingDetailsModal } from './BillingDetailsModal'
 
 type PrintModalProps = {
-  campaignId: string
+  campaignData?: CampaignData
   opened: boolean
+  invoiceData: {
+    invoiceDate: number
+    paymentDate: number
+    amount: number
+    currencyName: string
+  }
+  account: Account
   close: () => void
 }
 
-export const InvoicesModal = ({ campaignId, opened, close }: PrintModalProps) => {
+export const InvoicesModal = ({
+  campaignData,
+  invoiceData,
+  account,
+  opened,
+  close
+}: PrintModalProps) => {
   const { campaignsData } = useCampaignsData()
-  const {
-    adexAccount: {
-      billingDetails,
-      address,
-      refundsFromCampaigns: { perCampaign: refunds },
-      fundsOnCampaigns: { perCampaign: openings }
-    }
-  } = useAccount()
-
-  const campaignData = useMemo(
-    () => campaignsData.get(campaignId),
-
-    [campaignId, campaignsData]
-  )
-
   const campaign = useMemo(() => campaignData?.campaign, [campaignData])
 
-  const invoiceData = useMemo(() => {
-    const to = Number(campaign?.activeTo || 0)
+  // const invoiceData = useMemo(() => {
+  //   const to = Number(campaign?.activeTo || 0)
 
-    const campaignOpenData = openings.find((item) => item.id === campaign?.id)
-    const campaignCloseData = refunds.find((item) => item.id === campaign?.id)
-    const currencyName = campaignOpenData?.token.name || ''
-    const decimals = campaignOpenData?.token.decimals || 6
+  //   const campaignOpenData = openings.find((item) => item.id === campaign?.id)
+  //   const campaignCloseData = refunds.find((item) => item.id === campaign?.id)
+  //   const currencyName = campaignOpenData?.token.name || ''
+  //   const decimals = campaignOpenData?.token.decimals || 6
 
-    // NOTE: the actual payment is when the campaign is started (openings -> start dates, activeFrom is fallback)
-    // The question is: Is that ok from accounting stand point
-    // TODO: Should we have invoices for the full amount and credit notes for the refunds (on stop or expire with no full budget used)
-    const start = new Date(
-      campaignOpenData?.startDate || Number(campaign?.activeFrom) || 0
-    ).getTime()
+  //   // NOTE: the actual payment is when the campaign is started (openings -> start dates, activeFrom is fallback)
+  //   // The question is: Is that ok from accounting stand point
+  //   // TODO: Should we have invoices for the full amount and credit notes for the refunds (on stop or expire with no full budget used)
+  //   const start = new Date(
+  //     campaignOpenData?.startDate || Number(campaign?.activeFrom) || 0
+  //   ).getTime()
 
-    const end = new Date(campaignCloseData?.closeDate || to).getTime()
+  //   const end = new Date(campaignCloseData?.closeDate || to).getTime()
 
-    // TODO: discuss the payment and invoice date
+  //   // TODO: discuss the payment and invoice date
 
-    const amount = parseBigNumTokenAmountToDecimal(
-      BigInt(campaignOpenData?.amount || campaign?.campaignBudget || 0) -
-        BigInt(campaignCloseData?.amount || 0),
-      decimals
-    )
-    return {
-      invoiceDate: Math.min(end, to),
-      paymentDate: start,
-      amount,
-      currencyName
-    }
-  }, [
-    campaign?.activeFrom,
-    campaign?.activeTo,
-    campaign?.campaignBudget,
-    campaign?.id,
-    openings,
-    refunds
-  ])
+  //   const amount = parseBigNumTokenAmountToDecimal(
+  //     BigInt(campaignOpenData?.amount || campaign?.campaignBudget || 0) -
+  //       BigInt(campaignCloseData?.amount || 0),
+  //     decimals
+  //   )
+  //   return {
+  //     invoiceDate: Math.min(end, to),
+  //     paymentDate: start,
+  //     amount,
+  //     currencyName
+  //   }
+  // }, [
+  //   campaign?.activeFrom,
+  //   campaign?.activeTo,
+  //   campaign?.campaignBudget,
+  //   campaign?.id,
+  //   openings,
+  //   refunds
+  // ])
 
   // const campaignMappedAnalytics: BaseAnalyticsData[] | undefined = useMemo(
   //   () => mappedAnalytics.get(analyticsKey?.key || ''),
@@ -102,8 +100,8 @@ export const InvoicesModal = ({ campaignId, opened, close }: PrintModalProps) =>
       paymentDate: new Date(invoiceData.paymentDate),
       seller: ADEX_COMPANY_DETAILS,
       buyer: {
-        ...billingDetails,
-        ethAddress: address
+        ...account?.billingDetails,
+        ethAddress: account?.address
       },
       // TODO: Check if the value of VAT% should be greater than 0
       vatPercentageInUSD: 0,
@@ -119,13 +117,13 @@ export const InvoicesModal = ({ campaignId, opened, close }: PrintModalProps) =>
     campaignData?.clicks,
     campaignData?.ctr,
     campaignData?.avgCpm,
-    invoiceData.amount,
+    campaign?.id,
     invoiceData.invoiceDate,
     invoiceData.paymentDate,
     invoiceData.currencyName,
-    campaign?.id,
-    billingDetails,
-    address
+    invoiceData.amount,
+    account?.billingDetails,
+    account?.address
   ])
 
   return (
